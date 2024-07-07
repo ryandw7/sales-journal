ebdb9fa762368c94587f83e1b63ea33748023b3b
Ryan Wilson <ryandw77@gmail.com>
2024-07-07 20:16:36 +0000

    add dependencies and boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+console.log(path.resolve(__dirname, 'dist'));
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+
+        static: {
+            directory: path.join(__dirname, 'public'),
+          },
+          compress: true,
+          port: 9000,
+        },
+}
