2d8354077f9a47bdfe5d2a7403b05b3b5d151c36
Ryan Wilson <ryandw77@gmail.com>
2024-07-08 17:40:09 +0000

    fix dependency issue in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,6 @@ import {
   import Register from "./pages/Register";
 export default function App (){
     return (
-<<<<<<< HEAD
-        <h1>TESTY</h1>
-=======
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
@@ -19,6 +16,5 @@ export default function App (){
             <Route path="/register" element={<Register />} />
         </Routes>
         </BrowserRouter>
->>>>>>> ba7189b (add boiler plate for client, install dependencies)
     )
 }
