539eaff5f16e9c72c32302feff6833b93de88cc4
Ryan <ryandevlinwilson@gmail.com>
2024-07-08 20:39:35 -0400

    add Register functionality to client

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,23 +1,6 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 export default function Login() {
-    useEffect(() => {
-        const fetchProm = async () => {
-            try {
-                const req = await fetch('https://organic-space-telegram-xjq7wvv9qp4369qq-5000.app.github.dev/api', {
-                    mode: "no-cors"
-                });
-                console.log(req.json);
-                return req.json();
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        fetchProm();
-
-    })
     return (
         <div>
             <h1>Login</h1>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,5 +23,6 @@ module.exports = {
     },
     devServer: {
         port: 9000,
+        historyApiFallback: true
     },
 }
