485e8f394b688e2b46b70538c7d2071e275b53df
Ryan <ryandevlinwilson@gmail.com>
2024-07-14 20:49:57 -0400

    add jest coverage to testing

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom'
+    testEnvironment: 'jest-environment-jsdom',
+    collectCoverage: true,
+      coverageDirectory: "coverage",
+      coverageReporters: [
+        "html",
+        "text"
+      ]
        // ... other options ...
      }
