c1a13e8aa515715b1caffbff69b388bd7bf1f165
Ryan <ryandevlinwilson@gmail.com>
2024-07-18 13:58:05 -0400

    change .babelrc to babel.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    collectCoverage: true,
-      coverageDirectory: "coverage",
-      coverageReporters: [
-        "html",
-        "text"
-      ]
-       // ... other options ...
-     }
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    "html",
+    "text"
+  ]
+  // ... other options ...
+}
