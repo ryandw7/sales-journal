83d8baa07c483bdad1d3540dd33ac0b57f56ed3c
Ryan <ryandevlinwilson@gmail.com>
2024-07-18 14:46:46 -0400

    add jest.setup.js, jest.polyfills.js to counteract abundant errors with msw/jest combo, add mocks folder with handlers and server, test suite runs, no current implementation of msw yet

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
+  setupFiles: ['./jest.polyfills.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: [
     "html",
     "text"
-  ]
+  ],
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
   // ... other options ...
 }
---
+++ client/jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+import 'whatwg-fetch';
+
+import { server } from './src/mocks/server'
