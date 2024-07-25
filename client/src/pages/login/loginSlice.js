ac31f9d5939959256d29bd9143d67c9474580c70
Ryan Wilson <ryandw77@gmail.com>
2024-07-25 23:06:20 +0000

    start logic for fetchCredentials

--- client/src/pages/login/loginSlice.js
+++ client/src/pages/login/loginSlice.js
@@ -1,7 +1,21 @@
 import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchCredentials = createAsyncThunk('login/fetchCredentials', async (reqBody) => {
-    
+    try {
+        const { userName, password } = reqBody;
+        const res = await fetch('url', {
+            method: 'GET',
+            headers: {
+
+            },
+            body: JSON.stringify({
+               userName,
+               password
+            })
+        })
+    } catch (err) {
+        throw new Error()
+    }
 })
 const loginSlice = createReducer({
     name: 'login',
