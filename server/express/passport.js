1442dde11eddaaa499cbda4abe3aebeff85da6eb
Ryan <ryandevlinwilson@gmail.com>
2024-07-25 21:42:55 -0400

    update db utils to implement error handling, add passport functionality, and bcrypt to password generation

--- server/express/passport.js
+++ server/express/passport.js
@@ -1,17 +1,45 @@
 const passport = require("passport");
+const bcrypt = require("bcrypt")
 const LocalStrategy = require("passport-local").Strategy;
+const {findByUsername, findById}  = require('../db/dbUtils.js')
+passport.serializeUser((user, cb) => {
+  console.log("serializing...")
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  console.log('deserializing...')
+  findById(id, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
 
 passport.use(new LocalStrategy((username, password, cb)=>{
-    db.users.findByUsername(username, function (err, user) {
+
+    console.log("Authenticating user...")
+   
+    
+    findByUsername(username, async function (err, user) {
+      const matchedPassword = await bcrypt.compare(password, user.pw);
         if (err) {
+          console.log(err)
           return cb(err);
         }
         if (!user) {
+        console.log('!user')
           return cb(null, false);
         }
-        if (user.password != password) {
+        if (!matchedPassword) {
+          console.log(matchedPassword, password, user.pw)
           return cb(null, false);
         }
         return cb(null, user);
       })
 }))
+
+
+
+module.exports = passport;
