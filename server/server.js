fa993f00aa3b6f37442f157f1f39bf5424c39c0b
Ryan <ryandw77@gmail.com>
2024-09-02 13:56:19 -0400

    clean up server, remove unnecessary seperation of api.js and server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,34 @@
+// REQUIRED LIBRARIES:
+
+require("dotenv").config;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// REQUIRED MODULES
+
+const errorMiddleware = require('./express/middleware/errorMiddleware');
+
+const apiRouter = require('./express/routes/apiRouter');
+
+// SERVER VARIABLES
+
+const server = express();
 
 const PORT = process.env.SERVER_PORT || 4000;
-const express = require('express');
-const server = express();
-const apiRouter = require('./express/routes/apiRouter');
-const bodyParser = require('body-parser');
-const errorMiddleware = require('./express/middleware/errorMiddleware');
-const CLIENT_URL = require("dotenv").config;
+
+// SERVER SET UP
+
 server.use(express.static('public'));
+
 server.use(express.json());
+
 server.use(bodyParser.json());
+
 server.use('/api', apiRouter);
+
 server.use(errorMiddleware);
+
+// NPM RUN SERVE:
 
 server.listen(PORT, () => {
     console.log('Express server is listening on port: ' + PORT)
