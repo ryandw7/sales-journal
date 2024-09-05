bfa002428213d83f76cb6bb0724a228800a2578d
Ryan <ryandw77@gmail.com>
2024-09-04 20:09:56 -0400

    update .env

--- server/db/pgClient.js
+++ server/db/pgClient.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 require('dotenv').config()
+const { DATA_BASE, PG_HOST, PG_PORT, PG_USER, PG_PASSWORD } = process.env
 const client = new Client({
     database: 'clientlogtest',
     host: 'localhost',
